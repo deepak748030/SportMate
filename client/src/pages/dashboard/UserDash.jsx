fec4174b1bfc51eed935407727c3218ab8fb5a62
deepak748030 <deepakkushwah748930@gmail.com>
2024-07-07 21:49:26 +0530

    userdash

--- client/src/pages/dashboard/UserDash.jsx
+++ client/src/pages/dashboard/UserDash.jsx
@@ -1,122 +1,119 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from '../../components/layouts/Layout';
+import axios from 'axios';
+import apiUrl from '../../api/config';
+import { useAuth } from "../../context/auth";
 
 export default function UserDash() {
-    const userData = {
-        avatar: 'https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-1.png',
-        name: 'Robert Wolf',
-        job: 'Engineer',
-        email: 'rob_wolf@gmail.com',
-        phone: '+44 (452) 886 09 12',
-    };
+    const [auth] = useAuth();
+    const [myEventsData, setMyEventsData] = useState([]);
 
-    const myEventsData = [
-        {
-            _id: 1,
-            eventName: 'Football Tournament',
-            date: '2023-06-01',
-            time: '10:00 AM',
-            place: 'Stadium',
-            price: '100$',
-            numTeams: 10,
-            user: { firstName: 'John' },
-            winningPrize: '500$',
-        },
-        {
-            _id: 2,
-            eventName: 'Basketball Game',
-            date: '2023-06-02',
-            time: '12:00 PM',
-            place: 'Arena',
-            price: '50$',
-            numTeams: 8,
-            user: { firstName: 'Alice' },
-            winningPrize: '300$',
-        },
-    ];
+    useEffect(() => {
+        const fetchUserData = async () => {
+            try {
+                if (auth?.user?._id) {
+                    const eventsResponse = await axios.get(`${apiUrl}/user/${auth?.user?._id}`);
+                    setMyEventsData(eventsResponse.data || []);
+                }
+            } catch (error) {
+                console.error("Error fetching data", error);
+            }
+        };
+
+        fetchUserData();
+    }, [auth]);
 
     const handleEditEvent = (event) => {
-        // Handle event edit logic here
         console.log("Editing event:", event);
+    };
+
+    const handleDeleteEvent = async (eventId) => {
+        try {
+            await axios.delete(`${apiUrl}/events/${eventId}`);
+            setMyEventsData(myEventsData.filter(event => event._id !== eventId));
+        } catch (error) {
+            console.error("Error deleting event", error);
+        }
     };
 
     return (
         <Layout>
-            <div className="container-fluid bg-light p-4 md:p-8 lg:p-12">
+            <div className="container-fluid bg-light p-4">
                 <div className="container">
                     <div className="row">
-                        <div className="col-lg-8 col-sm-12 mb-4">
+                        <div className="col-lg-8 col-sm-12">
+                            <div className="row">
+                                {myEventsData.map((event) => (
+                                    <div className="col-12 col-md-6 mb-4" key={event._id}>
+                                        <div className="card h-100">
+                                            <img src={'https://media.istockphoto.com/id/1904589046/photo/two-adult-football-players-running-and-kicking-a-soccer-ball-legs-of-two-young-football.jpg?s=2048x2048&w=is&k=20&c=CSTkGc00q6A1VXG8YaHuBbLO58EeHFHkM5uEPoyMYZc='} alt="Event Banner" className="card-img-top" />
+                                            <div className="card-body">
+                                                <h5 className="card-title">{event.eventName}</h5>
+                                                <div className="row">
+                                                    <div className="col-6 d-flex align-items-center mb-2">
+                                                        <i className="bi bi-calendar-fill me-2"></i>
+                                                        <span>{event.date.slice(0, 10)}</span>
+                                                    </div>
+                                                    <div className="col-6 d-flex align-items-center mb-2">
+                                                        <i className="bi bi-clock-fill me-2"></i>
+                                                        <span>{event.time}</span>
+                                                    </div>
+                                                </div>
+                                                <div className="row">
+                                                    <div className="col-6 d-flex align-items-center mb-2">
+                                                        <i className="bi bi-geo-alt-fill me-2"></i>
+                                                        <span>{event.place}</span>
+                                                    </div>
+                                                    <div className="col-6 d-flex align-items-center mb-2">
+                                                        <i className="bi bi-currency-dollar me-2"></i>
+                                                        <span>{event.price}</span>
+                                                    </div>
+                                                </div>
+                                                <div className="row">
+                                                    <div className="col-6 d-flex align-items-center mb-2">
+                                                        <i className="bi bi-people-fill me-2"></i>
+                                                        <span>{event.participants.length} / {event.numTeams}</span>
+                                                    </div>
+                                                    <div className="col-6 d-flex align-items-center mb-2">
+                                                        <i className="bi bi-award-fill me-2"></i>
+                                                        <span>Prize: {event.winningPrize}</span>
+                                                    </div>
+                                                </div>
+                                                <div className="d-flex justify-content-between mt-2">
+                                                    <button className="btn btn-primary" onClick={() => handleEditEvent(event)}>Edit</button>
+                                                    <button className="btn btn-danger" onClick={() => handleDeleteEvent(event._id)}>Delete</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-sm-12">
                             <div className="card mb-4">
                                 <div className="card-body">
                                     <div className="d-flex align-items-center">
-                                        <img src={userData.avatar} className="rounded-circle me-3" alt={userData.name} style={{ width: '64px', height: '64px' }} />
+                                        <img src={auth?.user?.avatar} className="rounded-circle me-3" alt={auth?.user?.firstName} style={{ width: '64px', height: '64px' }} />
                                         <div>
-                                            <h5 className="card-title mb-1">{userData.name}</h5>
-                                            <p className="card-text text-muted">{userData.job}</p>
+                                            <h5 className="card-title mb-1">{`${auth?.user?.firstName} ${auth?.user?.lastName}`}</h5>
+                                            <p className="card-text text-muted">{auth?.user?.job}</p>
                                         </div>
                                     </div>
                                     <hr />
                                     <div className="d-flex flex-column flex-sm-row justify-content-between">
                                         <div className="d-flex align-items-center mb-2">
                                             <i className="bi bi-envelope-open-fill me-2"></i>
-                                            <span>{userData.email}</span>
+                                            <span>{auth?.user?.email}</span>
                                         </div>
                                         <div className="d-flex align-items-center mb-2">
                                             <i className="bi bi-phone-fill me-2"></i>
-                                            <span>{userData.phone}</span>
+                                            <span>{auth?.user?.phoneNumber}</span>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-
-                            {myEventsData.map(({ _id, date, eventName, numTeams, place, price, time, user, winningPrize }) => (
-                                <div className="card mb-4" key={_id} onClick={() => handleEditEvent({ _id, eventName, place, date, time, price, numTeams, user, winningPrize })}>
-                                    <div className="row g-0">
-                                        <div className="col-md-4">
-                                            <img src={'https://media.istockphoto.com/id/1904589046/photo/two-adult-football-players-running-and-kicking-a-soccer-ball-legs-of-two-young-football.jpg?s=2048x2048&w=is&k=20&c=CSTkGc00q6A1VXG8YaHuBbLO58EeHFHkM5uEPoyMYZc='} alt="Event Banner" className="img-fluid rounded" />
-                                        </div>
-                                        <div className="col-md-8">
-                                            <div className="card-body">
-                                                <h5 className="card-title">{eventName}</h5>
-                                                <div className="row">
-                                                    <div className="col-6 d-flex align-items-center mb-2">
-                                                        <i className="bi bi-calendar-fill me-2"></i>
-                                                        <span>{date.slice(0, 10)}</span>
-                                                    </div>
-                                                    <div className="col-6 d-flex align-items-center mb-2">
-                                                        <i className="bi bi-clock-fill me-2"></i>
-                                                        <span>{time}</span>
-                                                    </div>
-                                                </div>
-                                                <div className="row">
-                                                    <div className="col-6 d-flex align-items-center mb-2">
-                                                        <i className="bi bi-geo-alt-fill me-2"></i>
-                                                        <span>{place}</span>
-                                                    </div>
-                                                    <div className="col-6 d-flex align-items-center mb-2">
-                                                        <i className="bi bi-currency-dollar me-2"></i>
-                                                        <span>{price}</span>
-                                                    </div>
-                                                </div>
-                                                <div className="row">
-                                                    <div className="col-6 d-flex align-items-center mb-2">
-                                                        <i className="bi bi-people-fill me-2"></i>
-                                                        <span>{numTeams} / {numTeams}</span>
-                                                    </div>
-                                                    <div className="col-6 d-flex align-items-center mb-2">
-                                                        <i className="bi bi-person-fill me-2"></i>
-                                                        <span>{user?.firstName}</span>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-                        <div className="col-lg-4 col-sm-12">
                             <div className="card">
                                 <div className="card-body">
                                     <h5 className="card-title">Quick Links</h5>
