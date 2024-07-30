028c563405c3973de73d9d8a4257afc381d989a2
deepak748030 <deepakkushwah748930@gmail.com>
2024-07-30 20:41:05 +0530

    image change

--- client/src/pages/eventsperfomance/EventTeamStats.jsx
+++ client/src/pages/eventsperfomance/EventTeamStats.jsx
@@ -131,7 +131,7 @@ const EventTeamStats = () => {
                                     <div key={participant._id} className="col-md-4 mb-3">
                                         <div className="card h-100">
                                             <img
-                                                src={'https://media.istockphoto.com/id/1904589046/photo/two-adult-football-players-running-and-kicking-a-soccer-ball-legs-of-two-young-football.jpg?s=2048x2048&w=is&k=20&c=CSTkGc00q6A1VXG8YaHuBbLO58EeHFHkM5uEPoyMYZc=' || "https://via.placeholder.com/150"}
+                                                src={'https://plus.unsplash.com/premium_photo-1661963404614-74802f16a7a0?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' || "https://via.placeholder.com/150"}
                                                 alt="User"
                                                 className="card-img-top"
                                                 style={{ height: '230px', objectFit: 'cover' }}
